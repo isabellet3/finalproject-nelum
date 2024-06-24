4015ba96a72b791d9aa7c6100462d91f1cc22bb1
Isabelle Tuyay <iylabelle03@gmail.com>
2024-06-24 13:35:37 -0500

    Initial Commit

---
+++ finalproject-nelum/finalproject_nelumApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalproject_nelumApp.swift
+//  finalproject-nelum
+//
+//  Created by Scholar on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct finalproject_nelumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
