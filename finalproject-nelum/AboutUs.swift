7c6253a1f08c8557ce91e7a3977f2e484e2a2b4c
Isabelle Tuyay <iylabelle03@gmail.com>
2024-06-26 16:24:40 -0500

    About Us Front Page

--- finalproject-nelum/AboutUs.swift
+++ finalproject-nelum/AboutUs.swift
@@ -6,13 +6,134 @@
 //
 
 import SwiftUI
-
 struct AboutUs: View {
+    @State private var response = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack{
+                RoundedRectangle (cornerRadius:25)
+                    .fill(Color(red:234/255,green:172/255,blue:162/255))
+                    .frame(width:500, height:880)
+                RoundedRectangle (cornerRadius:25)
+                    .fill(Color("color-1"))
+                    .frame(width:355, height: 770)
+                ScrollView {
+                    VStack {
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Text("About the Creators")
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                            .padding()
+                            .font(.title)
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                      
+                        
+                    
+                            HStack {
+                        VStack {
+                            HStack {
+                                Image("image")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 175, height: 200)
+                                    .border(Color.white, width: 5)
+                                    .clipped()
+                                
+                                Image("image")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 175, height: 200)
+                                    .border(Color.white, width: 5)
+                                    .clipped()
+                                
+                            }
+                            HStack {
+                                NavigationLink(destination: GraceBio()) {
+                                    Text("Learn More About Grace")
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                        .padding()
+                                        .background(Color(red: 206/255, green: 106/255, blue: 107/255)
+                                        )
+                                        .cornerRadius(5.0)
+                                        .frame(width: 175, height: 100, alignment: .center)
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    NavigationLink(destination: IsabelleBio()) {
+                                        Text("Learn More About Isabelle")
+                                            .font(.headline)
+                                            .foregroundColor(.white)
+                                            .padding()
+                                            .background(Color(red: 206/255, green: 106/255, blue: 107/255)
+                                            )
+                                            .cornerRadius(5.0)
+                                            .frame(width: 175, height: 100, alignment: .center)
+                                    }}
+                            }
+                        HStack {
+                            VStack {
+                                HStack {
+                                    Image("image")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 175, height: 200)
+                                        .border(Color.white, width: 5)
+                                        .clipped()
+                                    
+                                    Image("image")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 175, height: 200)
+                                        .border(Color.white, width: 5)
+                                        .clipped()
+                            }
+                                HStack {
+                                    NavigationLink(destination: MariaBio()) {
+                                        Text("Learn More About Maria")
+                                            .font(.headline)
+                                            .foregroundColor(.white)
+                                            .padding()
+                                            .background(Color(red: 206/255, green: 106/255, blue: 107/255)
+                                        )
+                                            .cornerRadius(5.0)
+                                            .frame(width: 175, height: 100, alignment: .center)
+                                    
+                                    
+                                    NavigationLink(destination: ShriBio()) {
+                                        Text("Learn More About Shrinidhi")
+                                            .font(.headline)
+                                            .foregroundColor(.white)
+                                            .padding()
+                                            .background(Color(red: 206/255, green: 106/255, blue: 107/255)
+                                            )
+                                            .cornerRadius(5.0)
+                                            .frame(width: 175, height: 100, alignment: .center)
+                                    }}
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
-
 #Preview {
     AboutUs()
 }
+
---
+++ finalproject-nelum/IsabelleBio.swift
@@ -0,0 +1,18 @@
+//
+//  IsabelleBio.swift
+//  finalproject-nelum
+//
+//  Created by Scholar on 6/26/24.
+//
+
+import SwiftUI
+
+struct IsabelleBio: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IsabelleBio()
+}
